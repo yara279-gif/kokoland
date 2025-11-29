22493d557a8abd76c30256137c5aa97f37920cb4
yara279-gif <131772774+yara279-gif@users.noreply.github.com>
2025-11-30 00:05:43 +0200

    prepare to host on render

--- kokoland/settings.py
+++ kokoland/settings.py
@@ -266,6 +266,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        "rest_framework.permissions.AllowAny",
     ],
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
