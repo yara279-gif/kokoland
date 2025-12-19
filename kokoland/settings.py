3244bf57c341343e8112103727072c2107a93ee4
yara279-gif <131772774+yara279-gif@users.noreply.github.com>
2025-12-19 02:06:09 +0200

    edit CROS

--- kokoland/settings.py
+++ kokoland/settings.py
@@ -303,13 +303,7 @@ SIMPLE_JWT = {
 if IS_LOCAL:
     # Local development: Allow all origins
     CORS_ALLOW_ALL_ORIGINS = True
-    CORS_ALLOWED_ORIGINS = [
-        "https://kokoland.onrender.com",
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "http://localhost:8000",
-        "http://127.0.0.1:8000",
-    ]
+
 
 else:
     # Production: Specific origins only
